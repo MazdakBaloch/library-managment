30d14433b44ac3239f2ce5374522a14c0ebb5380
Muzmil Amin <134281771+MuzmilAmin@users.noreply.github.com>
2024-01-18 00:25:10 +0500

    initial comment

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1 class="text-primary">Hello wolrd</h1>
+</template>
+<script setup></script>
---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,47 @@
+<template>
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <SideBar />
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <TopBar />
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    
+                    <!-- Page Heading -->
+                    <slot />
+                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <Footer />
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    
+    <!-- End of Page Wrapper -->
+</template>
+<script setup>
+import Footer from './Footer.vue';
+import TopBar from './TopBar.vue';
+import SideBar from './SideBar.vue';
+</script>
